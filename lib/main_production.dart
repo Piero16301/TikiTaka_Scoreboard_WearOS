d3ed3986a50356460e5639039a26b3f5dddad71e
Piero Angelo Morales Alcalde <piero.morales@utec.edu.pe>
2025-03-22 15:26:59 -0500

    Se agrega Firebase

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,6 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:tiki_taka/app/app.dart';
 import 'package:tiki_taka/bootstrap.dart';
+import 'package:tiki_taka/firebase_options.dart';
 
-void main() {
-  bootstrap(() => const App());
+Future<void> main() async {
+  // Ensure Firebase is initialized
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await bootstrap(() => const App());
 }
