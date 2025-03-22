906fb4b5b997a8f40945d355bb945f08190011c0
Piero Angelo Morales Alcalde <piero.morales@utec.edu.pe>
2025-03-22 13:29:12 -0500

    Inicio del proyecto

---
+++ android/app/src/main/kotlin/com/pmorales/wearos/tikitaka/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pmorales.wearos.tikitaka
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:tiki_taka/app/app.dart';
+import 'package:tiki_taka/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
